9bfcad05aa831f21c5c9d79dc32e167d3938e4bc
ftptpf <inboxru@yandex.ru>
2021-08-19 09:43:08 +0300

    2. Тестовое задание [#504790] - Weak

--- src/main/java/ru/job4j/articles/service/SimpleArticleService.java
+++ src/main/java/ru/job4j/articles/service/SimpleArticleService.java
@@ -7,6 +7,7 @@ import ru.job4j.articles.model.Word;
 import ru.job4j.articles.service.generator.ArticleGenerator;
 import ru.job4j.articles.store.Store;
 
+import java.lang.ref.WeakReference;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
@@ -24,10 +25,25 @@ public class SimpleArticleService implements ArticleService {
     public void generate(Store<Word> wordStore, int count, Store<Article> articleStore) {
         LOGGER.info("Геренация статей в количестве {}", count);
         var words = wordStore.findAll();
+/*        for (int i = 0; i < count; i++) {
+            System.out.printf("Сгенерирована статья № %s", i);
+            System.out.println();
+            WeakReference<Article> articleWeak = new WeakReference<>(articleGenerator.generate(words));
+            articleStore.save(articleWeak.get());
+        }*/
         var articles = IntStream.iterate(0, i -> i < count, i -> i + 1)
                 .peek(i -> LOGGER.info("Сгенерирована статья № {}", i))
-                .mapToObj((x) -> articleGenerator.generate(words))
-                .collect(Collectors.toList());
-        articles.forEach(articleStore::save);
+                .mapToObj((x) -> new WeakReference(articleGenerator.generate(words)))
+                .collect(Collectors.toList()); //.forEach(articleStore.save(WeakReference::get));
+        for (WeakReference article : articles) {
+            if (article != null) {
+                articleStore.save((Article) article.get());
+            }
+        }
+
+        //articleStore.sa
+        //articles.forEach(WeakReference::get).;
+
+        //articles.forEach(articleStore::save);
     }
 }
