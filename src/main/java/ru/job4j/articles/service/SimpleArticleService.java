dfe1f2286ebe63e59893c11c99aeea4df9c550c8
ftptpf <inboxru@yandex.ru>
2021-08-20 10:07:32 +0300

    2. Тестовое задание [#504790] - for

--- src/main/java/ru/job4j/articles/service/SimpleArticleService.java
+++ src/main/java/ru/job4j/articles/service/SimpleArticleService.java
@@ -7,7 +7,10 @@ import ru.job4j.articles.model.Word;
 import ru.job4j.articles.service.generator.ArticleGenerator;
 import ru.job4j.articles.store.Store;
 
+import java.lang.ref.SoftReference;
 import java.lang.ref.WeakReference;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
@@ -23,23 +26,36 @@ public class SimpleArticleService implements ArticleService {
 
     @Override
     public void generate(Store<Word> wordStore, int count, Store<Article> articleStore) {
-        LOGGER.info("Геренация статей в количестве {}", count);
+        LOGGER.info("Генерация статей в количестве {}", count);
         var words = wordStore.findAll();
-/*        for (int i = 0; i < count; i++) {
-            System.out.printf("Сгенерирована статья № %s", i);
-            System.out.println();
+        int paket = 0;
+        for (int i = 0; i < count; i++) {
+            LOGGER.info("Сгенерирована статья № {}", i);
             WeakReference<Article> articleWeak = new WeakReference<>(articleGenerator.generate(words));
-            articleStore.save(articleWeak.get());
-        }*/
-        var articles = IntStream.iterate(0, i -> i < count, i -> i + 1)
+            List<SoftReference<Article>> softReferenceList = new ArrayList<>();
+            softReferenceList.add(new SoftReference<>(articleWeak.get()));
+            paket++;
+            if (paket == 100_000) {
+                for (SoftReference<Article> article : softReferenceList) {
+                    articleStore.save(article.get());
+                }
+                softReferenceList.clear();
+                paket = 0;
+            }
+        }
+        //var articles =
+/*                IntStream.iterate(0, i -> i < count, i -> i + 1)
                 .peek(i -> LOGGER.info("Сгенерирована статья № {}", i))
                 .mapToObj((x) -> new WeakReference(articleGenerator.generate(words)))
-                .collect(Collectors.toList()); //.forEach(articleStore.save(WeakReference::get));
-        for (WeakReference article : articles) {
+                .filter((x) -> x.get() != null)
+                .map(articleStore::save);*/
+                //.map((x) -> articleStore.save((Article) x.get()));
+                //.collect(Collectors.toList()); //.forEach(articleStore.save(WeakReference::get));
+/*        for (WeakReference article : articleWeak) {
             if (article != null) {
                 articleStore.save((Article) article.get());
             }
-        }
+        }*/
 
         //articleStore.sa
         //articles.forEach(WeakReference::get).;
